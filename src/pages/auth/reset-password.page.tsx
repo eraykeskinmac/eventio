a98b498afe54a27356777a92019991ff407f0651
eraykeskinmac <eraykeskinmac@gmail.com>
2024-03-10 05:33:49 +0300

    basic form improvements

--- src/pages/auth/reset-password.page.tsx
+++ src/pages/auth/reset-password.page.tsx
@@ -8,12 +8,38 @@ import { useRouter } from "next/router";
 import { useMutation } from "@blitzjs/rpc";
 import Link from "next/link";
 import { assert } from "blitz";
+import { useForm } from "@mantine/form";
+import { Button, PasswordInput, TextInput } from "@mantine/core";
 
 const ResetPasswordPage: BlitzPage = () => {
   const router = useRouter();
   const token = router.query.token?.toString();
   const [resetPasswordMutation, { isSuccess }] = useMutation(resetPassword);
 
+  const form = useForm({
+    initialValues: {
+      password: "",
+      passwordConfirmation: "",
+    },
+    validate: {},
+  });
+
+  let onSubmit = async (values) => {
+    try {
+      assert(token, "token is required.");
+      await resetPasswordMutation({ ...values, token });
+    } catch (error: any) {
+      if (error.name === "ResetPasswordError") {
+        return {
+          [FORM_ERROR]: error.message,
+        };
+      } else {
+        return {
+          [FORM_ERROR]: "Sorry, we had an unexpected error. Please try again.",
+        };
+      }
+    }
+  };
   return (
     <Layout title="Reset your password">
       <div>
@@ -27,38 +53,15 @@ const ResetPasswordPage: BlitzPage = () => {
             </p>
           </div>
         ) : (
-          <Form
-            submitText="Reset Password"
-            schema={ResetPassword}
-            initialValues={{
-              password: "",
-              passwordConfirmation: "",
-              token,
-            }}
-            onSubmit={async (values) => {
-              try {
-                assert(token, "token is required.");
-                await resetPasswordMutation({ ...values, token });
-              } catch (error: any) {
-                if (error.name === "ResetPasswordError") {
-                  return {
-                    [FORM_ERROR]: error.message,
-                  };
-                } else {
-                  return {
-                    [FORM_ERROR]: "Sorry, we had an unexpected error. Please try again.",
-                  };
-                }
-              }
-            }}
-          >
-            <LabeledTextField name="password" label="New Password" type="password" />
-            <LabeledTextField
-              name="passwordConfirmation"
-              label="Confirm New Password"
-              type="password"
+          <form onSubmit={form.onSubmit(onSubmit)}>
+            <PasswordInput withAsterisk label="Password" {...form.getInputProps("password")} />
+            <PasswordInput
+              withAsterisk
+              label="Confirm Password"
+              {...form.getInputProps("passwordConfirmation")}
             />
-          </Form>
+            <Button type="submit">Submit</Button>
+          </form>
         )}
       </div>
     </Layout>
